59eca17fc058c1201a99c7e37b03aa566169359d
JJ <infernoj2581@gmail.com>
2017-09-07 08:11:45 -0600

    Started the runner and controller file.

---
+++ sample/controller/SampleController.java
@@ -0,0 +1,8 @@
+package sample.controller;
+
+public class SampleController {
+	public void start() {
+		System.out.print("This is a sample project to show the github ");
+		System.out.print("eclipse process so I develop muscle memory");
+	}
+}
---
+++ sample/controller/SampleRunner.java
@@ -0,0 +1,7 @@
+package sample.controller;
+
+public class SampleRunner {
+	public static void main(String[]args) {
+
+	}
+}
